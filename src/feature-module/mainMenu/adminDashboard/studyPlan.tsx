86d911fd3d0ef7cbacf356b1a6ff39c2c8fbcc9f
Tanishq2730 <66459762+Tanishq2730@users.noreply.github.com>
2025-04-11 16:40:27 +0530

    code update

--- src/feature-module/mainMenu/adminDashboard/studyPlan.tsx
+++ src/feature-module/mainMenu/adminDashboard/studyPlan.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const StudyPlan: React.FC = () => {
   return (
     <div className="studyplantab">
-      <div className="card-body p-0">
+      {/* <div className="card-body p-0">
         <ul className="nav nav-tabs nav-tabs-solid nav-tabs-rounded mb-3" style={{justifyContent:'space-between'}}>
           <li className="nav-item">
             <Link
@@ -159,6 +159,102 @@ const StudyPlan: React.FC = () => {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      </div> */}
+      <div className="card-body px-sm-4 px-2">
+        <ul
+          className="nav nav-tabs justify-content-center mb-5 tab-style-3"
+          id="myTab2"
+          role="tablist"
+        >
+          <li className="nav-item me-0" role="presentation">
+            <button
+              className="nav-link active home py-1"
+              id="home-tab-1"
+              data-bs-toggle="tab"
+              data-bs-target="#home-tab-pane"
+              type="button"
+              role="tab"
+              aria-controls="home-tab-pane" 
+              aria-selected="true"
+            >
+              Speaking
+            </button>
+          </li>
+          <li className="nav-item me-0" role="presentation">
+            <button
+              className="nav-link about py-1"
+              id="profile-tab-2"
+              data-bs-toggle="tab"
+              data-bs-target="#profile-tab-pane"
+              type="button"
+              role="tab"
+              aria-controls="profile-tab-pane"
+              aria-selected="false"
+            >
+              Writing
+            </button>
+          </li>
+          <li className="nav-item me-0" role="presentation">
+            <button
+              className="nav-link services py-1"
+              id="contact-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#contact-tab-pane"
+              type="button"
+              role="tab"
+              aria-controls="contact-tab-pane"
+              aria-selected="false"
+            >
+              Reading
+            </button>
+          </li>
+          <li className="nav-item me-0" role="presentation">
+            <button
+              className="nav-link services py-1"
+              id="last-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#last-tab-pane"
+              type="button"
+              role="tab"
+              aria-controls="last-tab-pane"
+              aria-selected="false"
+            >
+              Listening
+            </button>
+          </li>
+        </ul>
+        <div className="tab-content" id="myTabContent1">
+          <div
+            className="tab-pane fade show active text-muted"
+            id="home-tab-pane"
+            role="tabpanel"
+            aria-labelledby="home-tab-1"
+            tabIndex={0}
+          >
+            <b>Lorem Ipsum is simply dummy text of the printing</b> and
+            typesetting industry. Lorem Ipsum has been the industry&apos;s.
+          </div>
+          <div
+            className="tab-pane fade text-muted"
+            id="profile-tab-pane"
+            role="tabpanel"
+            aria-labelledby="profile-tab-2"
+            tabIndex={0}
+          >
+            Many desktop publishing packages as their default model text, and a
+            search for <b>&apos;lorem ipsum&apos;</b> will over the years.
+          </div>
+          <div
+            className="tab-pane fade text-muted"
+            id="contact-tab-pane"
+            role="tabpanel"
+            aria-labelledby="contact-tab"
+            tabIndex={0}
+          >
+            There, <b>by injected humour</b>, or randomised words which
+            don&apos;t look even slightly believable.
           </div>
         </div>
       </div>
