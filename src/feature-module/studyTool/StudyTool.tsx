6f31b66bbe64281bfbe29054a14cb43cfc1838f0
Tanishq2730 <66459762+Tanishq2730@users.noreply.github.com>
2025-04-21 20:53:21 +0530

    my last updated code

--- src/feature-module/studyTool/StudyTool.tsx
+++ src/feature-module/studyTool/StudyTool.tsx
@@ -1,25 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  FaVideo,
-  FaFileAlt,
-  FaQuestionCircle,
-  FaFolder,
-  FaLink,
-  FaCalendarAlt,
-} from "react-icons/fa";
+  faVideo,
+  faFileAlt,
+  faQuestionCircle,
+  faFolder,
+  faLink,
+  faCalendarAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 type CardItem = {
-  icon: JSX.Element;
+  icon: any;
   title: string;
 };
 
 const cardData: CardItem[] = [
-  { icon: <FaVideo size={40} />, title: "Class Recording" },
-  { icon: <FaFileAlt size={40} />, title: "Template" },
-  { icon: <FaQuestionCircle size={40} />, title: "Grammar" },
-  { icon: <FaFolder size={40} />, title: "Prediction File" },
-  { icon: <FaLink size={40} />, title: "Class link" },
-  { icon: <FaCalendarAlt size={40} />, title: "Timetable" },
+  { icon: faVideo, title: "Class Recording" },
+  { icon: faFileAlt, title: "Template" },
+  { icon: faQuestionCircle, title: "Grammar" },
+  { icon: faFolder, title: "Prediction File" },
+  { icon: faLink, title: "Class link" },
+  { icon: faCalendarAlt, title: "Timetable" },
 ];
 
 const StudyTool: React.FC = () => {
@@ -30,11 +32,18 @@ const StudyTool: React.FC = () => {
           <div className="container">
             <div className="row my-4">
               {cardData.map((item, index) => (
-                <div className="col-12 col-sm-6 col-md-4 col-lg-3" key={index}>
-                  <div className="card p-4 text-center shadow-sm custom-card h-100">
-                    <div className="text-info mb-3 icon">{item.icon}</div>
-                    <h6 className="fw-semibold">{item.title}</h6>
-                  </div>
+                <div
+                  className="col-12 col-sm-6 col-md-4 col-lg-3 my-3"
+                  key={index}
+                >
+                  <Link to="#">
+                    <div className="card px-4 py-5 text-center mb-0">
+                      <div className="text-info mb-3 icon">
+                        <FontAwesomeIcon icon={item.icon} size="2x" />
+                      </div>
+                      <h6 className="fw-semibold">{item.title}</h6>
+                    </div>
+                  </Link>
                 </div>
               ))}
             </div>
